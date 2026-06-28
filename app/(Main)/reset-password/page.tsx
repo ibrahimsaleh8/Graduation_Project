6ff95c4233e25ee485fd17e3c2d5984981cd3115
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-06-28 18:55:00 +0300

    add suspense to reset pass page

--- app/(Main)/reset-password/page.tsx
+++ app/(Main)/reset-password/page.tsx
@@ -1,8 +1,19 @@
 import { Metadata } from "next";
 import ResetPassword from "./_components/ResetPassword";
+import { Spinner } from "@/components/ui/spinner";
+import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "Reset Password",
 };
 export default function ResetPassworPage() {
-  return <ResetPassword />;
+  return (
+    <Suspense
+      fallback={
+        <div className="w-full min-h-screen flex items-center justify-center">
+          <Spinner />
+        </div>
+      }>
+      <ResetPassword />
+    </Suspense>
+  );
 }
